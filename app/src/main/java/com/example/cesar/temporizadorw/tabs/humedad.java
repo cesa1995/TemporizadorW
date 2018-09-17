aa5e7981650aa0a9c60c5b8d921d41f78b7d200b
cesa1995 <cesar.contreras24777@gmail.com>
2018-09-17 19:52:13 -0400

    comenzando a editar humedad

--- app/src/main/java/com/example/cesar/temporizadorw/tabs/humedad.java
+++ app/src/main/java/com/example/cesar/temporizadorw/tabs/humedad.java
@@ -1,19 +1,34 @@
 package com.example.cesar.temporizadorw.tabs;
 
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
 import android.widget.NumberPicker;
+import android.widget.ProgressBar;
 import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import com.example.cesar.temporizadorw.comunication;
 
 import com.example.cesar.temporizadorw.R;
+import com.example.cesar.temporizadorw.conexion;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import static android.content.ContentValues.TAG;
 
 public class humedad extends Fragment {
 
@@ -24,6 +39,8 @@ public class humedad extends Fragment {
     NumberPicker hora, minutos, segundos;
     int relay;
     comunication mCallback;
+    ProgressBar cargando;
+    ArrayList<HashMap<String, String>> relayList;
 
 
     @Override
@@ -49,11 +66,120 @@ public class humedad extends Fragment {
         relayName = v.findViewById(R.id.relayName);
         saveBtn = v.findViewById(R.id.saveBtn);
         relay = 0;
+        cargando = v.findViewById(R.id.progressBar);
 
         mCallback.sendata(1,relay);
 
 
 
+
+    }
+
+    public class Gettimes extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected void onPreExecute() {
+
+            cargando.setVisibility(View.VISIBLE);
+
+        }
+
+        @Override
+        protected Void doInBackground(Void... arg0) {
+            conexion sh = new conexion();
+            // Making a request to url and getting response
+            String url = "http://192.168.4.1/info?id=stiotca&pass=1234567&tab=2";
+            String jsonStr = sh.makeServiceCall(url, "GET");
+
+            Log.e(TAG, "Response from url: " + jsonStr);
+            if (jsonStr != null) {
+                try {
+                    JSONObject jsonObj = new JSONObject(jsonStr);
+
+                    // Getting JSON Array node
+                    JSONArray contacts = jsonObj.getJSONArray("info");
+                    if (!relayList.isEmpty()) {
+                        //for (int i = 0; i < relayList.size(); i++) {
+                        //  relayList.remove(i);
+                        //}
+                        relayList.clear();
+                    }
+                    // looping through All Times
+                    for (int i = 0; i < contacts.length(); i++) {
+                        JSONObject c = contacts.getJSONObject(i);
+                        String nombre = c.getString("nombre");
+                        String timeINhh = c.getString("tin_HH");
+                        String timeONhh = c.getString("ton_HH");
+                        String timeOFFhh = c.getString("tof_HH");
+                        String timeFIhh = c.getString("tfi_HH");
+                        String timeINmm = c.getString("tin_MM");
+                        String timeONmm = c.getString("ton_MM");
+                        String timeOFFmm = c.getString("tof_MM");
+                        String timeFImm = c.getString("tfi_MM");
+                        String timeINss = c.getString("tin_SS");
+                        String timeONss = c.getString("ton_SS");
+                        String timeOFFss = c.getString("tof_SS");
+                        String timeFIss = c.getString("tfi_SS");
+
+                        // tmp hash map for single Times
+                        HashMap<String, String> relay = new HashMap<>();
+
+                        // adding each child node to HashMap key => value
+                        relay.put("nombre", nombre);
+                        relay.put("timeINhh", timeINhh);
+                        relay.put("timeINmm", timeINmm);
+                        relay.put("timeINss", timeINss);
+                        relay.put("timeONhh", timeONhh);
+                        relay.put("timeONmm", timeONmm);
+                        relay.put("timeONss", timeONss);
+                        relay.put("timeOFFhh", timeOFFhh);
+                        relay.put("timeOFFmm", timeOFFmm);
+                        relay.put("timeOFFss", timeOFFss);
+                        relay.put("timeFIhh", timeFIhh);
+                        relay.put("timeFImm", timeFImm);
+                        relay.put("timeFIss", timeFIss);
+
+                        // adding contact to contact list
+                        relayList.add(relay);
+
+                        System.out.println(relayList.toString());
+
+                        //entryArrayList= new ArrayList<>(relay.entrySet());
+                    }
+                } catch (final JSONException e) {
+                    Log.e(TAG, "error parsing datos: " + e.getMessage());
+                    getActivity().runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(getContext(),
+                                    "error parsing datos: " + e.getMessage(),
+                                    Toast.LENGTH_LONG).show();
+                        }
+                    });
+
+                }
+
+            } else {
+                Log.e(TAG, "No se reciben datos");
+                getActivity().runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(getContext(),
+                                "no se reciben datos del servidor",
+                                Toast.LENGTH_LONG).show();
+                    }
+                });
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            //selec();
+            mCallback.sendata(0, relay);
+            cargando.setVisibility(View.INVISIBLE);
+        }
 
     }
 
